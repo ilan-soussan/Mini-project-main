3240e60fa1b02d799b26b4ef69d8bb804368cbde
Didi-M1 <ymarashe@g.jct.ac.il>
2021-05-26 19:56:06 +0300

    viewPlane tringleTest

--- src/unitTests/ViewPlaneTests.java
+++ src/unitTests/ViewPlaneTests.java
@@ -19,7 +19,12 @@ public class ViewPlaneTests {
 
 	@Test
 	public void testConstructRayThroughPixelWithPlane() {
-	}
+        Camera Cam = new Camera(Point3D.ZERO,new Vector(0,0,-1),new Vector(0,1,0));
+        Cam.setViewPlaneSize(3,3);
+        Cam.setDistance(1);
+
+
+    }
 
 	@Test
 	public void testConstructRayThroughPixelWithTriangle() {
@@ -27,37 +32,31 @@ public class ViewPlaneTests {
 	    Cam.setViewPlaneSize(3,3);
 	    Cam.setDistance(1);
         triangle t1=new triangle(new Point3D(0, 1, -2),new Point3D(-1, -1, -2),new Point3D(1, -1, -2));
+
+        int count = findTheNumberOFIntsersectionsOnThePlane(Cam,t1);
+        assertEquals("Error with camere constructRayThroughPixel with triangle with one point",count,2);
+        //test 2
+	    triangle T2 = new triangle(new Point3D(0,20,-2),new Point3D(1,-1,-2),new Point3D(-1,-1,-2));
+
+	    count = findTheNumberOFIntsersectionsOnThePlane(Cam,T2);
+        assertEquals("Error with camere constructRayThroughPixel with triangle with more then one",count,1);
+    }
+    public int findTheNumberOFIntsersectionsOnThePlane(Camera Cam,Intersectable Shape)
+    {
         List<Point3D> tempList;
         int count = 0;
         for(int i=0;i<3;i++)
         {
             for (int j = 0;j<3;++j)
             {
-                tempList = t1.findIntsersections(Cam.constructRayThroughPixel(3,3,i,j));
+                tempList = Shape.findIntsersections(Cam.constructRayThroughPixel(3,3,i,j));
                 if(tempList!=null)
                 {
                     count +=tempList.size();
                 }
             }
         }
-        assertEquals("Error with camere constructRayThroughPixel with triangle with one point",count,2);
-        //test 2
-	    triangle T2 = new triangle(new Point3D(0,20,-2),new Point3D(1,-1,-2),new Point3D(-1,-1,-2));
-        tempList = null;
-        count = 0;
-        for(int i=0;i<3;i++)
-        {
-            for (int j = 0;j<3;++j)
-            {
-                tempList = T2.findIntsersections(Cam.constructRayThroughPixel(3,3,i,j));
-                if(tempList!=null)
-                {
-                    count +=tempList.size();
-                }
-            }
-        }
-        assertEquals("Error with camere constructRayThroughPixel with triangle with more then one",count,1);
+        return count;
     }
-
-
 }
+
