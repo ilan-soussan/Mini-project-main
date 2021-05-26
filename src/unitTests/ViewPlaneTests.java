5dd27311896e5ef63f5c18d977df524731cc38e7
Didi-M1 <ymarashe@g.jct.ac.il>
2021-05-26 17:47:40 +0300

    ViewPlane

---
+++ src/unitTests/ViewPlaneTests.java
@@ -0,0 +1,27 @@
+package unitTests;
+
+import primitives.*;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import elements.Camera;
+import geometries.*;
+
+public class ViewPlaneTests {
+
+	@Test
+	public void testConstructRayThroughPixelWithSphere() {
+	}
+
+	@Test
+	public void testConstructRayThroughPixelWithPlane() {
+	}
+
+	@Test
+	public void testConstructRayThroughPixelWithTriangle() {
+    }
+}
