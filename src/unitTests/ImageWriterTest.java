ac9feb2f927a669560784b7cfac12ea194944bb3
Didi-M1 <ymarashe@g.jct.ac.il>
2021-05-30 19:24:35 +0300

    Image Writer Test

--- src/unitTests/ImageWriterTest.java
+++ src/unitTests/ImageWriterTest.java
@@ -2,20 +2,28 @@ package unitTests;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import primitives.Color;
+import renderer.ImageWriter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ImageWriterTest {
 
-    @BeforeEach
-    void setUp() {
-    }
 
     @Test
     void writeToImage() {
+        ImageWriter img = new ImageWriter("Image",800,500);
+        for (int i=1;i<800;++i) {
+            for (int j = 1; j < 500; j++) {
+                if(j%50 ==0 || i%50==0)
+                    img.writePixel(i,j, new Color(150,80,60));
+                else
+                img.writePixel(i,j, new Color(200,150,150));
+
+            }
+        }
+        img.writeToImage();
+
     }
 
-    @Test
-    void writePixel() {
-    }
 }
